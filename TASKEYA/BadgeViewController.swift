464d8d4ecc5d58aeb602c18f80002d772fdf78bd
井上航 <k.prfm19@gmail.com>
2016-05-12 21:54:12 +0900

    first commit

--- TASKEYA/BadgeViewController.swift
+++ TASKEYA/BadgeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BadgeViewController.swift
 //  TASKEYA
 //
 //  Created by 井上航 on 2016/05/05.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BadgeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
+        
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
